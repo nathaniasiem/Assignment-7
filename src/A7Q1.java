9c58a15b8db1f3e552f84e665d18817681d876c7
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-05-02 10:12:15 -0400

    A7 initial commit with A7Q1

---
+++ src/A7Q1.java
@@ -0,0 +1,21 @@
+
+/**
+ *
+ * @author simon7323
+ */
+public class A7Q1 {
+    public static double circleArea(double radius){
+        //calculate the area of the circle
+        double rSquared=Math.pow(radius,2);
+        double area = rSquared*Math.PI;
+        return area;
+            }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create scanner to scan input
+        
+    }
+}
