a15bc7ffb05ca2249a069824f3693ee48db35254
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-05-03 09:33:00 -0400

    A7Q1-completed with comments

--- src/A7Q1.java
+++ src/A7Q1.java
@@ -1,13 +1,23 @@
 
+import java.util.Scanner;
+
+
 /**
  *
  * @author simon7323
  */
+
 public class A7Q1 {
+    /**
+     * calculates the area of the circle
+     * @param radius- radius of the circle
+     * @return 
+     */
     public static double circleArea(double radius){
         //calculate the area of the circle
         double rSquared=Math.pow(radius,2);
         double area = rSquared*Math.PI;
+        //send back answer
         return area;
             }
 
@@ -16,6 +26,17 @@ public class A7Q1 {
      */
     public static void main(String[] args) {
         // create scanner to scan input
+        Scanner input = new Scanner (System.in);
+        
+        //ask user to input the radius
+        System.out.println("Insert the radius of the circle: ");
+        int radius=input.nextInt();
+        
+        //calculate the area
+        double totalArea= circleArea(radius);
+        
+        //print out answer
+        System.out.println("The area of the circle is: "+totalArea);
         
     }
 }
