42da5ea56aeabb66edd77556a20633073f2e0e11
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-05-04 10:09:22 -0400

    A7Q3-completed

---
+++ src/A7Q3.java
@@ -0,0 +1,48 @@
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author simon7323
+ */
+public class A7Q3 {
+
+    /**
+     * Method for finding the factors of a number
+     *
+     * @param number -number to factor
+     * @return -print out the factors
+     */
+    public static int factor(int number) {
+
+        //set variable of starting factor
+        int factorNum = 1;
+
+        //create a loop to go through each number
+        System.out.println("Your factors for " + number + " are:");
+        while (factorNum <= number) {
+            //condition for no remainders in 
+            if (number % factorNum == 0) {
+                System.out.println(factorNum);
+            }
+            factorNum++;
+        }
+        return 0;
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create scanner to scan in input
+        Scanner input = new Scanner(System.in);
+
+        //ask user to input number to factor
+        System.out.println("Enter number you wish to factor: ");
+        int integer = input.nextInt();
+
+        //find the factors
+        int factorNumber = factor(integer);
+    }
+}
