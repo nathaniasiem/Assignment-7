02756c8f5a338f17b133e02e122c2038cea5fb04
simon7323 <simon7323@HRH-IC0027345.wrdsb.ca>
2017-05-08 10:14:26 -0400

    A7Q5-initial commit

---
+++ src/A7Q5.java
@@ -0,0 +1,20 @@
+
+
+/**
+ *
+ * @author simon7323
+ */
+public class A7Q5 {
+    
+    public static int chaotic(int line){
+        return 0;
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
