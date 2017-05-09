994600489c16b19458e64546eeb34f5a8f717023
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-05-09 10:08:00 -0400

    A7Q5-completed

--- src/A7Q5.java
+++ src/A7Q5.java
@@ -1,20 +1,45 @@
 
+import java.util.Scanner;
 
 /**
  *
  * @author simon7323
  */
 public class A7Q5 {
-    
-    public static int chaotic(int line){
-        return 0;
-        
+
+    /**
+     *
+     * @param line-number lines in the pattern
+     * @return
+     */
+    public static void chaotic(int line) {
+
+        //this generates the random number to create pattern
+        for (int i = 0; i < line; i++) {
+            int randNum = (int) (Math.random() * (5 - 1 + 1) + 1);
+            for (int j = 0; j < randNum; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println("");
+        }
+
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //create scanner to scan input
+        Scanner input = new Scanner(System.in);
+
+        //ask user to input number of line for the pattern
+        System.out.println("Enter the number of lines for the pattern: ");
+        int linePtrn = input.nextInt();
+
+        chaotic(linePtrn);
+
+
+
     }
 }
