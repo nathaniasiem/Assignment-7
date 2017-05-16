f44ab567194b1a2c06668df41dcbb13f882072dc
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-05-16 09:22:17 -0400

    A7Q9-Completed with comments

--- src/A7Q9.java
+++ src/A7Q9.java
@@ -1,49 +1,54 @@
 
 import java.util.Scanner;
 
-
 /**
  *
  * @author simon7323
  */
 public class A7Q9 {
+
     /**
      * method to determine if the number contains all odd numbers
+     *
      * @param num-number user inputs
-     * @return 
+     * @return
      */
- public static boolean allDigitsOdd(int num){
-     
-     boolean isOdd=true;
-      if (num==0){
-          isOdd=false; 
-         
-      } 
-      while(num!=0){
-          int digit = num%10;
-          if(digit%2==0){
-              isOdd=false;             
-          }
-          num/=10; 
-      }
-     return true;
-     
- }
+    public static boolean allDigitsOdd(int num) {
+
+        boolean isOdd = true;
+        if (num == 0) {
+            isOdd = false;
+        }
+        while (num != 0) {
+            int digit = num % 10;
+            if (digit % 2 == 0) {
+                isOdd = false;
+            }
+            num /= 10;
+        }
+        return isOdd;
+
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         //create scanner to scan input
-        Scanner input=new Scanner(System.in);
-        
+        Scanner input = new Scanner(System.in);
+
         //ask user to input a number
         System.out.println("Enter a number: ");
-        int integer =input.nextInt();
-        
+        int integer = input.nextInt();
+
         //call method allDigitsOdd
-        boolean result= allDigitsOdd(integer);
+        boolean result = allDigitsOdd(integer);
         
-        System.out.println("All your numbers are odd");
-        
+        //tell user whether or not their number includes all odd digits
+        if (result == true) {
+            System.out.println("All of the digits are odd");
+        } else {
+            System.out.println("Not all of the digits are odd.");
+        }
     }
 }
